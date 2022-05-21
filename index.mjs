500fd33b45c9f84de9d0fa0e055909b906eafc1f
Konrad Miekus <miekus.konrad@gmail.com>
2022-05-21 10:27:15 +0200

    serialize bundle

--- index.mjs
+++ index.mjs
@@ -7,7 +7,6 @@ import fs from 'fs'
 import { cpus } from 'os'
 import { dirname, join, resolve } from 'path'
 import { fileURLToPath } from 'url'
-import { DependencyResolver } from 'jest-resolve-dependencies'
 
 const root = join(dirname(fileURLToPath(import.meta.url)), 'product')
 
@@ -30,33 +29,66 @@ const entryPoint = resolve(process.cwd(), options.entryPoint)
 if (!hasteFS.exists(entryPoint))
   throw new Error('`--entry-point` does not exist. Please provide a path to a valid file.')
 
+console.log(chalk.bold(`❯ Building ${chalk.blue(options.entryPoint)}`))
+
 const resolver = new Resolver.default(moduleMap, {
   extensions: ['.js'],
   hasCoreModules: false,
   rootDir: root,
 })
 
-const dependencyResolver = new DependencyResolver(resolver, hasteFS)
-// this will resolve only the entryPoint's dependencies. We need to resolve it recursively.
-const allFiles = new Set() // that's a list of all the files we'll include in the bundle
+const seen = new Set()
+const modules = new Map()
 const queue = [entryPoint]
 while (queue.length) {
   const module = queue.shift()
 
-  if(allFiles.has(module)) continue // a guard against circular dependencies
+  if(seen.has(module)) continue // a guard against circular dependencies
 
-  allFiles.add(module)
-  queue.push(...dependencyResolver.resolve(module))
+  seen.add(module)
+  // entry-point: Map<string, string>
+  // ['./apple', 'path/to/apple.js']
+  const dependencyMap = new Map(
+    hasteFS
+      .getDependencies(module)
+      .map((dependencyName) => [
+        dependencyName,
+        resolver.resolveModule(module, dependencyName),
+      ]),
+  )
+  const code = fs.readFileSync(module, 'utf8')
+  const moduleBody = code.match(/module\.exports\s+=\s+(.*?);/)?.[1] || '' //whatever is exported from the module
+
+  const metadata = {
+    code: moduleBody || code,
+    dependencyMap,
+  }
+  modules.set(module, metadata)
+  queue.push(...dependencyMap.values())
 }
-console.log(chalk.bold(`❯ Found ${chalk.blue(allFiles.size)} files`))
-console.log(Array.from(allFiles))
-console.log(chalk.bold(`❯ Building ${chalk.blue(options.entryPoint)}`))
+console.log(modules)
+console.log(chalk.bold(`❯ Found ${chalk.blue(seen.size)} files`));
 console.log(chalk.bold(`❯ Serializing bundle`))
 
-const allCode = []
-await Promise.all(Array.from(allFiles).map(async (file) => {
-  const code = await fs.promises.readFile(file, 'utf8')
-  allCode.push(code)
-}))
+// we're processing the dependency map in the reverse order. So the first element in the reversed map won't have any deps
+// then we're moving on, and we're just replacing the require statements with the code that's in the files they require
+// thus we know that all the dependencies are resolved the moment we start processing them. The final result will have
+// no require statements anywhere
+for (const [module, metadata] of Array.from(modules).reverse()) {
+   let { code } = metadata
 
-console.log(allCode.join('\n'))
+   for (const [dependencyName, dependencyPath] of metadata.dependencyMap) {
+     // Inline the module body of the dependency into the module that requires it.
+     code = code.replace(
+       new RegExp(
+         // Escape `.` and `/`.
+        `require\\(('|")${dependencyName.replace(/[\/.]/g, '\\$&')}\\1\\)`,
+        ),
+        modules.get(dependencyPath).code,
+    )
+  }
+
+  metadata.code = code
+}
+
+console.log(modules.get(entryPoint).code)
