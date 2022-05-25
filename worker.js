4b7ebaae7a0016e69e0a0fbd7343031ddfbfd07a
Konrad Miekus <miekus.konrad@gmail.com>
2022-05-25 07:38:57 +0200

    add comments

--- worker.js
+++ worker.js
@@ -1,9 +1,11 @@
 const { transformSync } = require('@babel/core')
 
+// we're gonna run this in parallel. Transformation/compilation is CPU heavy process that's easy to parallelize
 exports.transformFile = function (code) {
   const transformResult = { code: '' }
   try {
     transformResult.code = transformSync(code, {
+      // because of this plugin we can mix require with imports and not care about *.mjs files
       plugins: ['@babel/plugin-transform-modules-commonjs'],
     }).code
   } catch (error) {
